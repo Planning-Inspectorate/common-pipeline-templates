25db3b0b902d873c2541c62333027d6180562921
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-01-13 15:43:09 +0000

    use script file for syntax highlighting

--- steps/azure_container_registry_purge.sh
+++ steps/azure_container_registry_purge.sh
@@ -4,23 +4,24 @@
 # echo to >&2 for stderr, for logs
 # echo to stdout for function output
 
-app_services_in_sub() {
+set_subscription() {
   local subscription_id=$1
+  echo "setting subscription to ${subscription_id}"
   az account set --subscription "$subscription_id"
   if [ $? -ne 0 ]; then
       echo "Failed to set subscription: $subscription_id" >&2
       exit 1
   fi
+}
 
-  echo "fetching app services for ${subscription_id}" >&2
+app_services_in_sub() {
+  echo "fetching app services"
   app_services=$(az webapp list --query "[].{name: name, resourceGroup: resourceGroup}" -o tsv)
   if [ $? -ne 0 ]; then
-      echo "Failed to list web apps for subscription: $subscription_id" >&2
+      echo "Failed to list web apps for subscription" >&2
       exit 1
   fi
-  echo "app services for ${subscription_id}" >&2
-  echo "${app_services}" >&2
-  echo "" >&2
+  echo "app services:"
   echo "$app_services"
 }
 
@@ -29,7 +30,7 @@ image_tag_name() {
   local resource_group=$2
   local slot=$3
 
-  echo "fetch tag name for ${name} ${resource_group} ${slot}" >&2
+  echo "fetch tag name for ${name} ${resource_group} ${slot}"
 
   # get config
   local container_image_name=$(az webapp config container show --name "$name" --resource-group "$resourceGroup" --slot "$slot")
@@ -40,7 +41,7 @@ image_tag_name() {
 
   # find the image name
   container_image=$(echo $container_image_name | jq -r '.[] | select(.name == "DOCKER_CUSTOM_IMAGE_NAME").value' | awk -F'|' '{print $2}')
-  echo "tag name for ${name} ${resource_group} ${slot} is ${container_image}" >&2
+  echo "tag name for ${name} ${resource_group} ${slot} is ${container_image}"
   echo "$container_image"
 }
 
@@ -52,7 +53,7 @@ images_in_sub() {
     exit 1
   fi
 
-  echo "fetch images for sub ${subscription_id}" >&2
+  echo "fetch images for sub ${subscription_id}"
 
   while IFS=$'\t' read -r name resource_group; do
     # default image tag
@@ -75,7 +76,6 @@ images_in_sub() {
   echo "$in_use_images"
 }
 
-
 # pull variables from the environment
 # ACR_SUBSCRIPTION_ID - subscription ID for the ACR
 # APP_SERVICE_SUBSCRIPTION_IDS - JSON list of subscription IDs
@@ -87,36 +87,65 @@ else
     acr_subscription_id=${ACR_SUBSCRIPTION_ID}
 fi
 
+if [ -z "${ACR_NAME}" ]; then
+    acr_name="pinscrsharedtoolinguks"
+else
+    acr_name=${ACR_NAME}
+fi
+
 if [ -z "${APP_SERVICE_SUBSCRIPTIONS}" ]; then
     app_service_subscription_ids=$(echo '["962e477c-0f3b-4372-97fc-a198a58e259e","76cf28c6-6fda-42f1-bcd9-6d7dbed704ef","dbfbfbbf-eb6f-457b-9c0c-fe3a071975bc","d1d6c393-2fe3-40af-ac27-f5b6bad36735"]' | jq -r '.[]')
 else
     app_service_subscription_ids=$(echo "$APP_SERVICE_SUBSCRIPTION_IDS" | jq -r '.[]')
 fi
+
 repositories=$(echo "$REPOSITORIES" | jq -r '.[]')
+
+echo "acr sub id:"
+echo "$acr_subscription_id"
+echo "acr name:"
+echo "$acr_name"
+echo "Subs:"
+echo "${app_service_subscription_ids[@]}"
+echo "Repositories:"
+echo "${repositories[@]}"
 
 # run the script
 
-echo "ACR cleanup. Repositories:"
-echo ""
-echo "Repositories:"
-echo "${repositories[@]}"
-echo ""
-echo "Subs:"
-echo "${app_service_subscription_ids[@]}"
-echo ""
-
+# get a list of images currently in use
 in_use_images=()
 while read -r subscription_id; do
-  tags=$(images_in_sub "$subscription_id")
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
+  set_subscription "$subscription_id" || exit 1
+  tags=$(images_in_sub) || exit 1
   in_use_images+=( $tags )
 done <<< "$app_service_subscription_ids" # pass sub ids (one per line) to read
 
 echo "Images:"
 echo "${in_use_images[@]}"
 
-## TODO: read images, determine list to delete
+# login to acr
+echo "switching to acr subscription"
+set_subscription "$acr_subscription_id" || exit 1
+echo "logging in to acr"
+az acr login --name "${acr_name}" || exit 1
+
+for repository in "${repositories[@]}"; do
+  all_tags=$(az acr repository show-tags --name "${acr_name}" --repository "$repository" --output tsv)
+  IFS=$'\n' read -r -d '' -a all_tags_array <<< "$all_tags"
+
+  images_to_purge=()
+  for tag in "${all_tags_array[@]}"; do
+    if ! [[ " ${in_use_images[@]} " =~ " ${acr_name}.azurecr.io/$repository:$tag " ]]; then
+      images_to_purge+=("$repository:$tag")
+    fi
+  done
+
+  echo "$repository include: ${images_to_purge[@]}"
+
+  for tag in "${images_to_purge[@]}"; do
+    echo "delete --image $tag"
+    # az acr repository delete --name "${acr_name}" --image "$tag" --yes || exit 1
+  done
+done
 
 echo "ACR cleanup completed"
